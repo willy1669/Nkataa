fb1449a81b7fbdcf21246e1cca94335fbd523c78
chinonso williams <williamschinonso11@gmail.com>
2018-06-26 12:57:25 +0100

    Corrected my posts on my userschema

--- Models/User.js
+++ Models/User.js
@@ -4,7 +4,7 @@ var UserSchema = mongoose.Schema({
     name: String,
     email: {type: String, unique: true},
     password: String,
-    posts:[{type:mongoose.Schema.Types.ObjectID, ref: 'post'}]
+    posts:[{type:mongoose.Schema.Types.ObjectId, ref: 'post'}]
 })
 
 module.exports = mongoose.model('User', UserSchema);
