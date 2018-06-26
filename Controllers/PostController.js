9a39081ebe14e4bd4721f5f8521a7c90dbf5bdbf
chinonso williams <williamschinonso11@gmail.com>
2018-06-26 18:31:55 +0100

    added my addPost function

---
+++ Controllers/PostController.js
@@ -0,0 +1,16 @@
+var model = require('../Models/Post');
+
+exports.addPost = function(req, res){
+    var data = {
+        time: Date.now(),
+        postBody: req.body.postBody,
+        user: req.body.user,
+    };
+    model.create(data, function(err){
+        if(err) res.json({err: err, message: 'Post could not be created'})
+        res.json({message:'Post was created successfully'})
+    });
+}
+
+
+
