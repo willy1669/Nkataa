d0dea096c13171164eec54f6958a94c368ebec68
chinonso williams <williamschinonso11@gmail.com>
2018-06-26 12:36:02 +0100

    added my userController to my User route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../Controllers/UserController');
 
 /* GET users listing. */
 router.post('/create', userController.addUser);
