378da969402c92b4690480d7abf4085ad73a3c83
chinonso williams <williamschinonso11@gmail.com>
2018-06-26 12:22:09 +0100

    added my routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+Router.post('/create', userController.addUser);
+
+Router.get('/', userController.getUser);
+
+Router.get('/delete/:id', userController.deleteUser);
 
 module.exports = router;
