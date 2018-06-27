e4c16b83297e79d9a23652a68fb3c915c7a1e0d6
chinonso williams <williamschinonso11@gmail.com>
2018-06-27 05:37:08 +0100

    added my posts and comments route

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ mongoose.connect('mongodb://localhost:27017/nkataa');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
