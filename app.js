85e2025696512d6e6381342e0d7025bcc4927657
chinonso williams <williamschinonso11@gmail.com>
2018-06-27 05:21:55 +0100

    added my postsRouter

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
